7207c4994fc880628415cb721400b155ec20fd6e
victor truong <2truong93@gmail.com>
2019-03-29 09:08:09 -0400

    first commit

---
+++ schema.sql
@@ -0,0 +1,9 @@
+create table users (
+    id serial primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar (500),
+    coffee varchar(500),
+    flavor varchar(500)
+);
+
---
+++ seed.sql
@@ -0,0 +1,7 @@
+insert into users
+    (first_name, last_name, email, coffee, flavor)
+values
+    ('Lady', 'Godiva', 'chocobessed@hotmail.com', 'mocha', 'chocolate'),
+    ('Nicolas', 'Flamel', 'tomorrowneverdies@netscape.net', 'dark roast', 'alchemical'),
+    ('Cruella', 'DeVille', 'puppypower@yahoo.com', 'frappuchino', 'white chocolate'),
+    ('Lennie', 'Small', 'bunnybuddy@outlook.com', 'french press', 'strawberry')
